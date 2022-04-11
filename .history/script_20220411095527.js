452ce0c9634c6413fd3e5a26c114380da2dee4c5
Stuckens <kelian2009@hotmail.fr>
2022-04-11 10:32:45 +0200

    ajout

---
+++ .history/script_20220411095527.js
@@ -0,0 +1,13 @@
+let uploadBtn = document.querySelector("#file-recto");
+uploadBtn.addEventListener("change", changeBG);
+function changeBG() {
+  let reader;
+  if (this.files && this.files[0]) {
+    reader = new FileReader();
+    reader.onload = (e) => {
+      bgObject.img.src = e.target.result;
+      drawCanvas();
+    };
+    reader.readAsDataURL(this.files[0]);
+  }
+}
